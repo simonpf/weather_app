51f611629aff04ccce2a2d57d63b90562f96e240
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-09-15 22:07:42 +0200

    Change test to temperature prediction instead of precipitation.

--- test/test_weather_app.py
+++ test/test_weather_app.py
@@ -1,17 +1,17 @@
 """Tests for the weather_app main module."""
-from weather_app import get_precipitation_forecast
+from weather_app import get_temperature_forecast
 
-def test_precipitation_forecast():
+def test_temperature_forecast():
     """
-    Ensure consistency of precipitation forecast by comparing 2h-resolution
+    Ensure consistency of temperature forecast by comparing 2h-resolution
     forecast to a sub-sampled 1h-resolution forecast.
     """
     hours = range(24)
-    forecast_1h = get_precipitation_forecast(hours)
+    forecast_1h = get_temperature_forecast(hours)
     assert len(forecast_1h) == 24
 
     hours = range(0, 24, 2)
-    forecast_2h = get_precipitation_forecast(hours)
+    forecast_2h = get_temperature_forecast(hours)
     assert len(forecast_2h) == 12
 
     assert all(forecast_1h[::2] == forecast_2h)
