8dc523db1c4c96a8c354adc9f76f7652929639e5
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-09-15 21:16:45 +0200

    Added placeholder for get_precipitation_forecast().

---
+++ weather_app/__init__.py
@@ -0,0 +1,13 @@
+"""
+weather_app
+===========
+
+The weather_app package provides functions to access SMHI weather
+forecasts.
+"""
+
+def get_precipitation_forecast():
+    """
+    So far just a placeholder.
+    """
+    pass
